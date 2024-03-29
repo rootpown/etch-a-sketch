0e7ac3d7d5cbd56e9929181f1193d9d2fdb53ca7
⇨ Arman  ⇨ 21 y.o <rootpown@gmail.com>
2024-03-30 01:50:21 +0400

    изменение компонентов

--- script.js
+++ script.js
@@ -3,8 +3,47 @@ const BTNCLEAR = document.getElementById("btn-clear");
 const BTNRANDOM = document.getElementById("btn-random");
 const BTNCOLOR = document.getElementById("btn-color");
 const elementBOX = document.getElementsByClassName("elementBOX");
+const GRIDCONT = document.getElementById("grid-content");
+const CLEARBUTTON = document.createElement("Button");
+
 let gridtemp = 600;
-let gridSize = 16;
+let gridSize = 10;
+let currentColor = "#000";
+
+function elementGrid() {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const div = document.createElement("div");
+      div.classList.add("elementBOX");
+      GRIDBOX.append(div);
+      div.addEventListener("mousedown", function (event) {
+        drawWithHold(event);
+      });
+      div.addEventListener("mouseover", function (event) {
+        if (event.buttons === 1) {
+          drawWithHold(event);
+        }
+      });
+    }
+  }
+  CLEARBUTTON.classList.add("button-clear");
+  CLEARBUTTON.textContent = "CLEAR";
+  GRIDCONT.append(CLEARBUTTON);
+  CLEARBUTTON.addEventListener("click", () => {
+    for (let i = 0; i < elementBOX.length; i++) {
+      elementBOX[i].style.backgroundColor = "#fff";
+    }
+  });
+  for (let i = elementBOX.length - 1; i >= 0; i--) {
+    elementBOX[i].style.width = gridtemp / gridSize + "px";
+    elementBOX[i].style.height = gridtemp / gridSize + "px";
+  }
+}
+function preventDefaultMouseDown(event) {
+  event.preventDefault();
+}
+document.addEventListener("mousedown", preventDefaultMouseDown);
+
 function randomColor() {
   let color = "#";
   for (let i = 0; i < 3; ++i) {
@@ -14,29 +53,21 @@ function randomColor() {
   }
   return color;
 }
-function elementGrid() {
-  for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
-      const div = document.createElement("div");
-      div.classList.add("elementBOX");
-      GRIDBOX.append(div);
-      div.addEventListener(
-        "mouseover",
-        () => (div.style.backgroundColor = "#000")
-      );
-      BTNCLEAR.addEventListener("click", () => {
-        div.style.backgroundColor = "#fff";
-      });
-      let RGB = randomColor();
-      div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = RGB;
-      });
-    }
-  }
-  for (let i = elementBOX.length - 1; i >= 0; i--) {
-    elementBOX[i].style.width = gridtemp / gridSize + "px";
-    elementBOX[i].style.height = gridtemp / gridSize + "px";
-  }
+
+function drawWithHold(event) {
+  event.target.style.backgroundColor = currentColor;
 }
+
+BTNRANDOM.addEventListener("click", function () {
+  currentColor = randomColor();
+});
+
+BTNCOLOR.addEventListener("click", function () {
+  currentColor = "#000";
+});
+
+BTNCLEAR.addEventListener("click", function () {
+  currentColor = "#fff";
+});
 
 elementGrid();
